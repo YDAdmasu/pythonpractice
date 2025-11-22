8373dfd94a943e294b57b0575438459c7df008d9
YDAdmasu <yididiyaadmasu@gmail.com>
2025-11-22 10:55:37 +0300

    O(n) time complexity

--- square.py
+++ square.py
@@ -5,3 +5,8 @@ def square(n):
         square.append(i*i)
     return square
 print(square(n))
+#using list comprehension
+n = [1,2,3,4,5]
+square = [i*i for i in n]
+print(square)
+# this an example of O(n) time complexity
